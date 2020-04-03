c6ccec7dd44c1223e8fc766781333a0317308f63
Ishaan Jain <ishaan160799@gmail.com>
2020-04-03 23:56:56 +0530

    Font files added and Splash Activty complete

--- app/src/main/java/com/ironclad1607/ardirectory/MainActivity.kt
+++ app/src/main/java/com/ironclad1607/ardirectory/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.ironclad1607.ardirectory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
+
+    private var mDelayHandler: Handler? = null
+    private var SPLASH_DELAY: Long = 5000
+
+    private val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mDelayHandler = Handler()
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+    }
+
+
+    override fun onDestroy() {
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+        super.onDestroy()
     }
 }
