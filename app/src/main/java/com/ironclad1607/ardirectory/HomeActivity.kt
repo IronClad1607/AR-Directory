3c9c5da599963a6fd5389052b724f56b691c0e0e
Ishaan Jain <ishaan160799@gmail.com>
2020-04-04 20:21:39 +0530

    Caterogies linked with home

--- app/src/main/java/com/ironclad1607/ardirectory/HomeActivity.kt
+++ app/src/main/java/com/ironclad1607/ardirectory/HomeActivity.kt
@@ -1,12 +1,33 @@
 package com.ironclad1607.ardirectory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ironclad1607.ardirectory.caterogies.AnimalActivity
+import com.ironclad1607.ardirectory.caterogies.CarActivity
+import com.ironclad1607.ardirectory.caterogies.PlanetActivity
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        cvAnimals.setOnClickListener {
+            val animalIntent = Intent(this, AnimalActivity::class.java)
+            startActivity(animalIntent)
+        }
+
+        cvPlanets.setOnClickListener {
+            val planetIntent = Intent(this, PlanetActivity::class.java)
+            startActivity(planetIntent)
+        }
+
+        cvCars.setOnClickListener {
+            val carIntent = Intent(this,CarActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/ironclad1607/ardirectory/caterogies/AnimalActivity.kt
+++ app/src/main/java/com/ironclad1607/ardirectory/caterogies/AnimalActivity.kt
@@ -2,6 +2,7 @@ package com.ironclad1607.ardirectory.caterogies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ironclad1607.ardirectory.R
 
 class AnimalActivity : AppCompatActivity() {
 
