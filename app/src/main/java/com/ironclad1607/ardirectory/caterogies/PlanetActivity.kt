6f8c8d504f0c638d7a7ae915485a96eb0ad0fede
Ishaan Jain <ishaan160799@gmail.com>
2020-04-06 16:26:43 +0530

    Planet Activity list donr

--- app/src/main/java/com/ironclad1607/ardirectory/caterogies/PlanetActivity.kt
+++ app/src/main/java/com/ironclad1607/ardirectory/caterogies/PlanetActivity.kt
@@ -2,12 +2,31 @@ package com.ironclad1607.ardirectory.caterogies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ironclad1607.ardirectory.ListAdapter
 import com.ironclad1607.ardirectory.R
+import kotlinx.android.synthetic.main.activity_animal.*
+import kotlinx.android.synthetic.main.activity_planet.*
 
 class PlanetActivity : AppCompatActivity() {
+    private val planetArray = arrayOf(
+        "Mercury",
+        "Venus",
+        "Earth",
+        "Mars",
+        "Jupiter",
+        "Saturn",
+        "Neptune",
+        "Uranus"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_planet)
+
+        rvPlanets.apply {
+            layoutManager = LinearLayoutManager(this@PlanetActivity)
+            adapter = ListAdapter(planetArray)
+        }
     }
 }
